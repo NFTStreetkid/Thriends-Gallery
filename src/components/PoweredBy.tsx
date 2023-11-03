9ac197cf2e91a1150a6bfe6e4fdd1729df68660d
NFTStreetkid <149784942+NFTStreetkid@users.noreply.github.com>
2023-11-03 23:37:11 +0000

    commit

--- src/components/PoweredBy.tsx
+++ src/components/PoweredBy.tsx
@@ -5,7 +5,7 @@ import type { FC } from "react";
 export const PoweredBy: FC = () => {
   return (
     <a
-      href={`https://thirdweb.com/${chain}/${contractAddress}`}
+      href={`https://thriends.io/${chain}/${contractAddress}`}
       target="_blank"
       rel="noreferrer"
       className="mr-4 flex max-w-[163px] cursor-pointer items-center justify-center gap-3 rounded-lg bg-white/5 px-4 py-2 shadow-2xl md:ml-auto"
@@ -13,14 +13,14 @@ export const PoweredBy: FC = () => {
       <img
         className="h-4 w-6 object-contain"
         src="/thirdweb.svg"
-        alt="thirdweb"
+        alt="SKT"
       />
       <div className="flex flex-col">
         <p className="text-xs font-semibold text-white">
           {truncateAddress(contractAddress)}
         </p>
         <p className="text-[8px] font-bold text-white/50">
-          powered by thirdweb
+          THRIENDS
         </p>
       </div>
     </a>
