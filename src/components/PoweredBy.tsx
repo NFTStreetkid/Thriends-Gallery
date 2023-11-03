be7480d3b7cef903a51ff0162b6d942c4d2796fb
NFTStreetkid <149784942+NFTStreetkid@users.noreply.github.com>
2023-11-03 23:42:06 +0000

    commit

--- src/components/PoweredBy.tsx
+++ src/components/PoweredBy.tsx
@@ -5,7 +5,7 @@ import type { FC } from "react";
 export const PoweredBy: FC = () => {
   return (
     <a
-      href={`https://thriends.io/${chain}/${contractAddress}`}
+      href={`https://thriends.io/`}
       target="_blank"
       rel="noreferrer"
       className="mr-4 flex max-w-[163px] cursor-pointer items-center justify-center gap-3 rounded-lg bg-white/5 px-4 py-2 shadow-2xl md:ml-auto"
@@ -16,9 +16,6 @@ export const PoweredBy: FC = () => {
         alt="SKT"
       />
       <div className="flex flex-col">
-        <p className="text-xs font-semibold text-white">
-          {truncateAddress(contractAddress)}
-        </p>
         <p className="text-[8px] font-bold text-white/50">
           THRIENDS
         </p>
