f44ce10d25d3687beab22fe08439402f390931cb
NFTStreetkid <149784942+NFTStreetkid@users.noreply.github.com>
2023-11-03 23:23:54 +0000

    commit

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,7 +2,7 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractAddress = "0xed5af388653567af2f388e6224dc7c4b3241c544";
+export const contractAddress = "0x324706Edc0490eCBcD0db41D4e779913cC7f4cf1";
 
 // The name of the chain your contract is deployed to.
 // Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
